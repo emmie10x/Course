0f0a41ab4cdac173903641679cdba6a0723657e9
emmie10x <ssingh03604@gmail.com>
2026-03-02 23:29:12 +0530

    Added Queue Implementation

---
+++ Queue/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    cout<<"Size of queue: "<<q.size()<<endl;
+
+    cout<<"Front element: "<<q.front()<<endl;
+    cout<<"Back element: "<<q.back()<<endl;
+
+    q.pop();
+    cout<<"After popping one element, front element: "<<q.front()<<endl;
+
+    return 0;
+}
